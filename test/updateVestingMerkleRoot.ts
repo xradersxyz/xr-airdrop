f98d42f403b77aa6d243b311a601a97d866ff505
junyeong-choi <jychoi927@gmail.com>
2024-06-26 09:48:17 +0900

    add updateVestingMerkleRoot script

---
+++ test/updateVestingMerkleRoot.ts
@@ -0,0 +1,34 @@
+import { ethers } from "hardhat";
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import fs from "fs";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("updateMerkleRoot start...");
+  const contractAddress =
+    process.env.MAIN_NET == "1"
+      ? (process.env.VESTING_CONTRACT_ADDRESS_MAINNET as string)
+      : (process.env.VESTING_CONTRACT_ADDRESS_TESTNET as string);
+
+  const tree = StandardMerkleTree.load(
+    JSON.parse(fs.readFileSync("tree-xp-vesting-dev.json", "utf8"))
+  );
+
+  const MerkleTreeClaim = await ethers.getContractAt(
+    "MerkleTreeClaim",
+    contractAddress
+  );
+
+  console.log("tree.root", tree.root);
+
+  const merkleRoot = await MerkleTreeClaim.updateMerkleRoot(tree.root);
+  console.log("updateMerkleRoot done.", merkleRoot);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
