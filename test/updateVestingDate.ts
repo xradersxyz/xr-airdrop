bc87692db54916ad9c87c04adaf41c372671d1db
kimandy7778 <kimandy7778@gmail.com>
2024-07-04 09:40:31 +0900

    add update vesting date .ts file modify contract name

--- test/updateVestingDate.ts
+++ test/updateVestingDate.ts
@@ -5,25 +5,28 @@ import fs from "fs";
 async function main() {
   const [deployer] = await ethers.getSigners();
 
-  console.log("updateMerkleRoot start...");
+  console.log("update vesting date start...");
   const contractAddress =
     process.env.MAIN_NET == "1"
       ? (process.env.VESTING_CONTRACT_ADDRESS_MAINNET as string)
       : (process.env.VESTING_CONTRACT_ADDRESS_TESTNET as string);
-
-  const tree = StandardMerkleTree.load(
-    JSON.parse(fs.readFileSync("tree-xp-vesting-dev.json", "utf8"))
-  );
 
   const MerkleTokenVesting = await ethers.getContractAt(
     "MerkleTokenVesting",
     contractAddress
   );
 
-  console.log("tree.root", tree.root);
+  const startDate = new Date("2024-07-12T00:00:00Z");
+  const start = Math.floor(startDate.getTime() / 1000); // Set specified start date relative to UTC
+  const cliff = 0; // Cliff period: 60 * 60 * 24 * 90 90 days
+  const duration = 60 * 60 * 24 * 153; // Total vesting period: 90 days
 
-  const merkleRoot = await MerkleTokenVesting.updateMerkleRoot(tree.root);
-  console.log("updateMerkleRoot done.", merkleRoot);
+  const vestingDate = await MerkleTokenVesting.updateVestingDate(
+    start,
+    cliff,
+    duration
+  );
+  console.log("update vesting date done.", vestingDate);
 }
 
 main()
